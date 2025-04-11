92392f83ee5daf73a35581fb6752850d55a16876
Burak Ezer <burakezerrr@gmail.com>
2025-04-11 15:33:36 +0300

    fix

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,12 @@
 function getElementWidth(content, padding, border) {
-    return parseFloat(content)+parseFloat(padding)*2+parseFloat(border)*2+"px";
+    return (
+        parseFloat(content) +
+        parseFloat(padding) * 2 +
+        parseFloat(border) * 2 +
+        'px'
+    )
 }
 
-
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+console.log(getElementWidth('50px', '8px', '4px'))
+console.log(getElementWidth('60px', '12px', '8.5px'))
+console.log(getElementWidth('200px', '0px', '0px'))
